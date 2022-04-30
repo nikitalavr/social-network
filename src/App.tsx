ed3909b86ec08e12e36f7ff0d90a23866d56d264
Nikita Lavrentev <nlavrentev93@gmail.com>
2022-04-30 15:34:30 +0400

    First commit, changed src from existing project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
+import Technologis from "./Technologies";
 
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+        <Header />
+        <Technologis/>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/Technologies.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Technologis = () => {
+    return (
+        <div className="App">
+            Simple HTML
+            <div>
+                <ul>
+                    <li>css</li>
+                    <li>html</li>
+                    <li>js</li>
+                    <li>react</li>
+                </ul>
+            </div>
+            <div>second div</div>
+        </div>
+    )
+}
+
+export default Technologis;
